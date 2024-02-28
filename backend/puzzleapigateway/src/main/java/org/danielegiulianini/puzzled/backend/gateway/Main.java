22730fb101d62baf3cdefa0476d89ebd8eacf5c6
Daniele Giulianini <daniele.giulianini3@studio.unibo.it>
2024-02-28 14:28:29 +0100

    add initial gradle' multi-project configuration

---
+++ backend/puzzleapigateway/src/main/java/org/danielegiulianini/puzzled/backend/gateway/Main.java
@@ -0,0 +1,7 @@
+package org.danielegiulianini.puzzled.backend.gateway;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ backend/puzzlemanagementservice/src/main/java/org/danielegiulianini/puzzled/backend/managementservice/Main.java
@@ -0,0 +1,8 @@
+package org.danielegiulianini.puzzled.backend.managementservice;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+
+}
